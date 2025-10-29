e7cb1a6bd0cb700296e710811e41db6f2815b4f1
Alex Barker <alex.barker234@gmail.com>
2025-10-29 11:26:07 +0800

    removed back

--- apps/web/src/components/itemPage/ItemPageSkeleton.tsx
+++ apps/web/src/components/itemPage/ItemPageSkeleton.tsx
@@ -1,9 +1,11 @@
+import { getStandaloneCookieServer } from "@/lib/utils/serverCookies";
 import BackNav from "../BackNav";
 
-export default function ItemPageSkeleton({ children }: { children: React.ReactNode }) {
+export default async function ItemPageSkeleton({ children }: { children: React.ReactNode }) {
+  const isStandalone = await getStandaloneCookieServer();
   return (
     <div className="mx-auto flex w-full max-w-4xl flex-col gap-5 px-2 py-4 lg:px-8">
-      <BackNav />
+      {!isStandalone && <BackNav />}
       {children}
     </div>
   );
