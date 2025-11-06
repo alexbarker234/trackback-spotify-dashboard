673dec52860c17c7014300a2b298fa4eae99bdc5
Alex Barker <alex.barker234@gmail.com>
2025-11-06 09:28:01 +0800

    styled react calendar

--- apps/web/src/app/dashboard/test/page.tsx
+++ apps/web/src/app/dashboard/test/page.tsx
@@ -1,8 +1,11 @@
+import CalendarRangeSelector from "@/components/calendar/CalendarRangeSelector";
+
 export default async function TestPage() {
   return (
     <div className="min-h-screen p-8">
       <div className="mx-auto flex max-w-7xl flex-col gap-y-6">
         <h1 className="mb-8 text-3xl font-bold text-white">Chart Testing</h1>
+        <CalendarRangeSelector />
       </div>
     </div>
   );
---
+++ apps/web/src/components/calendar/CalendarRangeSelector.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { formatDate } from "@/lib/utils/timeUtils";
+import { useState } from "react";
+import Calendar from "react-calendar";
+import styles from "./calendar.module.css";
+
+type CalendarRangeSelectorProps = {
+  onChange?: (value: [Date, Date]) => void;
+};
+
+export default function CalendarRangeSelector({ onChange }: CalendarRangeSelectorProps) {
+  const [value, setValue] = useState<[Date, Date] | null>(null);
+
+  const onInternalChange = (value: Date | Date[]) => {
+    if (Array.isArray(value)) {
+      const valueArray = value as [Date, Date];
+      setValue(valueArray);
+      onChange?.(valueArray);
+    }
+  };
+
+  return (
+    <div className={styles.calendarContainer}>
+      <Calendar selectRange={true} onChange={onInternalChange} />
+      {/* Display the selected range */}
+      <div className="mt-4 rounded-lg bg-white/5 p-4 backdrop-blur-sm">
+        {value ? (
+          <p className="text-center text-sm text-white">
+            {formatDate(value[0].getTime())} - {formatDate(value[1].getTime())}
+          </p>
+        ) : (
+          <p className="text-center text-sm text-gray-400">No range selected</p>
+        )}
+      </div>
+    </div>
+  );
+}
